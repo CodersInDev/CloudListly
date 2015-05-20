6710b6a6016ec69855e1dffdc88b09e7be881002
SimonLab <simon.labondance@gmail.com>
2015-05-20 12:36:02 +0100

    log on search

--- js/search.js
+++ js/search.js
@@ -2,6 +2,7 @@ function doSearch() {
 	$("#result").empty();
 	var searched = document.getElementById("searchterm").value;
     SC.get('/tracks', { q: searched }, function(tracks) {
+			console.log(tracks);
     $(tracks).each(function(index, track) {
       $('#result').append($('<li></li>').html(track.title));
     });
