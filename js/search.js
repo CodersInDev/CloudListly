075198bfed1a58f13ca6e8f467cc355a1ef51ea6
rub1e <alex.rubner@gmail.com>
2015-05-21 17:30:03 +0100

    fixed player and added error message to bad search

--- js/search.js
+++ js/search.js
@@ -1,26 +1,21 @@
 var buttonSearch = document.getElementById("buttonSearch");
+var searchBox = document.getElementById("searchterm");
 
-buttonSearch.addEventListener("click", doSearch);
-
-function doSearch() {
+buttonSearch.addEventListener("click", function() {
 	var result = document.getElementById("result");
 	result.innerHTML = "";
 	var searchResults = "";
-	var searched = document.getElementById("searchterm").value;
-    SC.get('/tracks', { q: searched }, function(tracks) {
-	    for (var i = 0; i < tracks.length; i++) {
-			//result.innerHTML += "<li class='tracks'>" + tracks[i].title + "</li>";
-			var classStyle = "";
-			if(tracks[i].streamable ==  false){
-				classStyle = 'nonStreamable';
-				//track.className += track.className ? ' nonStreamable' : 'nonStreamable';
-			}
-			if(tracks[i].downloadable ==  true){
-				classStyle = 'downloadable';
-				//track.className += track.className ? ' downloadable' : 'downloadable';
-			}
-			searchResults += "<li id=" + tracks[i].id +" class=" + classStyle +">" + tracks[i].title + "</li>";
+
+	myPlayer.searchList(searchBox.value, function (list) {
+		if(!list.length) {
+			result.insertAdjacentHTML('beforeend', "<h3>No songs match your search query - try to have more mainstream taste next time</h3>")
 		}
-		result.insertAdjacentHTML('beforeend', searchResults);
-  });
-}
+		for (var i = 0; i < list.length; i++) {
+			if(list[i].streamable){
+				searchResults += "<li id=" + list[i].id +">" + list[i].title + "</li>";
+			};
+			result.insertAdjacentHTML('beforeend', searchResults);
+		}
+	});	
+});
+
