30dd15d8b018cf8e0fcd14e4e88c964dde248fa6
SimonLab <simon.labondance@gmail.com>
2015-05-26 22:54:46 +0100

    reorganization!

--- js/navigationTabs.js
+++
@@ -1,8 +0,0 @@
-function playlistClick() {
-   document.getElementById('listTracks').style.display = "block";
-    document.getElementById('searchContent').style.display = 'none';
-}
-function searchClick() {
-document.getElementById('searchContent').style.display = 'block';
-    document.getElementById('listTracks').style.display ="none";
-}
